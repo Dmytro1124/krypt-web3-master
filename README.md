972543ff6dd9f669834434eee76d54e320d6c216
bunny-bet <weronos.business@gmail.com>
2024-10-20 11:26:36 +0900

    dev branch register

--- README.md
+++
@@ -1,2 +0,0 @@
-# Krypt - Web 3.0 Blockchain Application
-![Krypt](https://i.ibb.co/DVF4tNW/image.png)
--- client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from "./Transactions.json";
-
-export const contractAddress = "0xDb13edC67ca695812987e325974774A1Fc167a38";
-export const contractABI = abi.abi;
